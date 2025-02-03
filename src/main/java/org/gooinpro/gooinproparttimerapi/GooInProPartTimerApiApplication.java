794a57b04fd28d470f6e5d4af8bf1ae2f8edbaaf
dasol <ekthf988172@gmail.com>
2025-02-03 13:53:27 +0900

    first commit

---
+++ src/main/java/org/gooinpro/gooinproparttimerapi/GooInProPartTimerApiApplication.java
@@ -0,0 +1,13 @@
+package org.gooinpro.gooinproparttimerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooInProPartTimerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GooInProPartTimerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gooinpro/gooinproparttimerapi/GooInProPartTimerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gooinpro.gooinproparttimerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooInProPartTimerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
