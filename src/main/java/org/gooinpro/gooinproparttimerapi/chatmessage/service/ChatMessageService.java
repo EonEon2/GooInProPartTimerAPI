4cb2d15a4da5e6a20a85f2273ad4e05a11ba6574
jhyeong <hyeongcode@gmail.com>
2025-02-05 15:11:46 +0900

    feat: message save service add

--- src/main/java/org/gooinpro/gooinproparttimerapi/chatmessage/service/ChatMessageService.java
+++ src/main/java/org/gooinpro/gooinproparttimerapi/chatmessage/service/ChatMessageService.java
@@ -2,9 +2,14 @@ package org.gooinpro.gooinproparttimerapi.chatmessage.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.gooinpro.gooinproparttimerapi.chatmessage.domain.ChatMessageEntity;
+import org.gooinpro.gooinproparttimerapi.chatmessage.dto.ChatMessageDTO;
 import org.gooinpro.gooinproparttimerapi.chatmessage.repository.ChatMessageRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.Date;
 
 @Service
 @Log4j2
@@ -14,5 +19,19 @@ public class ChatMessageService {
 
     private final ChatMessageRepository chatMessageRepository;
 
+    public ChatMessageEntity saveMessageService(ChatMessageDTO chatMessageDTO) {
 
+        ChatMessageEntity chatMessageEntity = ChatMessageEntity.builder()
+                .roomId(chatMessageDTO.getRoomId())
+                .senderEmail(chatMessageDTO.getSenderEmail())
+                .message(chatMessageDTO.getMessage())
+                .sentAt(Date.from(Instant.now()))
+                .build();
+
+        ChatMessageEntity savedMessage = chatMessageRepository.save(chatMessageEntity);
+
+        log.info("Successfully saved Chat Message");
+
+        return savedMessage;
+    }
 }
