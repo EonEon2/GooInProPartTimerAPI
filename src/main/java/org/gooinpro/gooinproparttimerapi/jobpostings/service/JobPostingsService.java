6a62df3ad22fae3eeec046535d4469dda6410d72
EonEon2 <duqdjs123@naver.com>
2025-02-06 14:36:07 +0900

    feat: job posting list

---
+++ src/main/java/org/gooinpro/gooinproparttimerapi/jobpostings/service/JobPostingsService.java
@@ -0,0 +1,26 @@
+package org.gooinpro.gooinproparttimerapi.jobpostings.service;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.gooinpro.gooinproparttimerapi.common.dto.PageRequestDTO;
+import org.gooinpro.gooinproparttimerapi.common.dto.PageResponseDTO;
+import org.gooinpro.gooinproparttimerapi.jobpostings.dto.JobPostingsListDTO;
+import org.gooinpro.gooinproparttimerapi.jobpostings.repository.JobPostingsRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Log4j2
+public class JobPostingsService {
+
+    private final JobPostingsRepository jobPostingsRepository;
+
+    public PageResponseDTO<JobPostingsListDTO> JobPostingsList(PageRequestDTO pageRequestDTO) {
+
+        return jobPostingsRepository.jobPostingsList(pageRequestDTO);
+
+    }
+}
