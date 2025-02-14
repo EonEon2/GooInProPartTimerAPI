3a9208940825ec363665487a0f1f52fa3276b98d
EonEon2 <duqdjs123@naver.com>
2025-02-14 14:58:45 +0900

    feat: JobPostingsApplication Add

--- src/main/java/org/gooinpro/gooinproparttimerapi/jobpostings/repository/search/JobPostingsSearch.java
+++ src/main/java/org/gooinpro/gooinproparttimerapi/jobpostings/repository/search/JobPostingsSearch.java
@@ -2,13 +2,19 @@ package org.gooinpro.gooinproparttimerapi.jobpostings.repository.search;
 
 import org.gooinpro.gooinproparttimerapi.common.dto.PageRequestDTO;
 import org.gooinpro.gooinproparttimerapi.common.dto.PageResponseDTO;
+import org.gooinpro.gooinproparttimerapi.jobpostings.domain.JobPostingsEntity;
 import org.gooinpro.gooinproparttimerapi.jobpostings.dto.JobPostingDetailDTO;
 import org.gooinpro.gooinproparttimerapi.jobpostings.dto.JobPostingsListDTO;
+import org.gooinpro.gooinproparttimerapi.parttimer.domain.PartTimerEntity;
+
+import java.util.Optional;
 
 public interface JobPostingsSearch {
 
     PageResponseDTO<JobPostingsListDTO> jobPostingsList(PageRequestDTO pageRequestDTO);
 
     JobPostingDetailDTO jobPostingDetail(Long jpno);
+
+
 
 }
