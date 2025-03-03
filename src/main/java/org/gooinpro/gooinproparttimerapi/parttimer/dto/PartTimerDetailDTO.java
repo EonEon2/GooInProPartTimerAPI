39e0228ece51b641abc3136cbe0d89ee3dbc5aac
jhyeong <hyeongcode@gmail.com>
2025-03-03 17:07:22 +0900

    clore: before checkout

--- src/main/java/org/gooinpro/gooinproparttimerapi/parttimer/dto/PartTimerDetailDTO.java
+++ src/main/java/org/gooinpro/gooinproparttimerapi/parttimer/dto/PartTimerDetailDTO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.sql.Date;
 import java.sql.Timestamp;
 
 @Data
@@ -14,7 +16,7 @@ public class PartTimerDetailDTO {
     private Long pno;
     private String pemail;
     private String pname;
-    private Timestamp pbirth;
+    private Date pbirth;
     private boolean pgender;
     private String proadAddress;
     private String pdetailAddress;
