9ee4f489d8c64f88a70d9db48730ce413246c2b9
jhyeong <hyeongcode@gmail.com>
2025-03-04 12:24:58 +0900

    feat: chat part remove

--- src/main/java/org/gooinpro/gooinproparttimerapi/chatroom/repository/ChatRoomRepository.java
+++
@@ -1,7 +0,0 @@
-package org.gooinpro.gooinproparttimerapi.chatroom.repository;
-
-import org.gooinpro.gooinproparttimerapi.chatroom.domain.ChatRoomEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ChatRoomRepository extends MongoRepository<ChatRoomEntity, String> {
-}
