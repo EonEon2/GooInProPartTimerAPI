41ecba1b8ff7ba7d47c898e76fc793f0a0eaad18
EonEon2 <duqdjs123@naver.com>
2025-03-05 14:35:50 +0900

    feat: in out work log end

--- src/main/java/org/gooinpro/gooinproparttimerapi/worklogs/dto/WorkLogsInDTO.java
+++ src/main/java/org/gooinpro/gooinproparttimerapi/worklogs/dto/WorkLogsInDTO.java
@@ -1,12 +1,16 @@
 package org.gooinpro.gooinproparttimerapi.worklogs.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class WorkLogsInDTO {
 
     private Timestamp wlstartTime; // 출근시간
--- src/main/java/org/gooinpro/gooinproparttimerapi/worklogs/repository/search/WorkLogsSearchImpl.java
+++ src/main/java/org/gooinpro/gooinproparttimerapi/worklogs/repository/search/WorkLogsSearchImpl.java
@@ -13,9 +13,7 @@ import org.gooinpro.gooinproparttimerapi.parttimer.domain.PartTimerEntity;
 import org.gooinpro.gooinproparttimerapi.parttimer.repository.PartTimerRepository;
 import org.gooinpro.gooinproparttimerapi.worklogs.domain.QWorkLogsEntity;
 import org.gooinpro.gooinproparttimerapi.worklogs.domain.WorkLogsEntity;
-import org.gooinpro.gooinproparttimerapi.worklogs.dto.WorkLogsDTO;
-import org.gooinpro.gooinproparttimerapi.worklogs.dto.WorkLogsInOutDTO;
-import org.gooinpro.gooinproparttimerapi.worklogs.dto.WorkLogsTimeDTO;
+import org.gooinpro.gooinproparttimerapi.worklogs.dto.*;
 import org.gooinpro.gooinproparttimerapi.worklogs.repository.WorkLogsRepository;
 import org.gooinpro.gooinproparttimerapi.worklogs.service.WorkLogsService;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
@@ -23,6 +21,9 @@ import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 import java.sql.Time;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -90,4 +91,58 @@ public class WorkLogsSearchImpl extends QuerydslRepositorySupport implements Wor
                 .fetch();
     }
 
+    @Override
+    public WorkLogsInDTO getTodayStartTimeStatus(Long pno, Long jmno) {
+        QWorkLogsEntity workLogs = QWorkLogsEntity.workLogsEntity;
+
+        LocalDate today = LocalDate.now();
+        LocalDateTime startOfDay = today.atStartOfDay();
+        LocalDateTime endOfDay = today.atTime(LocalTime.MAX);
+
+        JPQLQuery<WorkLogsEntity> query = from(workLogs);
+        query.where(workLogs.pno.pno.eq(pno));
+        query.where(workLogs.jobMatching.jmno.eq(jmno));
+        query.where(workLogs.wlstartTime.between(
+                Timestamp.valueOf(startOfDay),
+                Timestamp.valueOf(endOfDay)
+        ));
+
+        JPQLQuery<WorkLogsInDTO> tupleQuery = query.select(
+                Projections.bean(WorkLogsInDTO.class,
+                        workLogs.wlstartTime,
+                        workLogs.wlworkStatus)
+        );
+
+        WorkLogsInDTO result = tupleQuery.fetchOne();
+
+        return result;
+    }
+
+    @Override
+    public WorkLogsOutDTO getTodayEndTimeStatus(Long pno, Long jmno) {
+        QWorkLogsEntity workLogs = QWorkLogsEntity.workLogsEntity;
+
+        LocalDate today = LocalDate.now();
+        LocalDateTime startOfDay = today.atStartOfDay();
+        LocalDateTime endOfDay = today.atTime(LocalTime.MAX);
+
+        JPQLQuery<WorkLogsEntity> query = from(workLogs);
+
+        query.where(workLogs.pno.pno.eq(pno));
+        query.where(workLogs.jobMatching.jmno.eq(jmno));
+        query.where(workLogs.wlstartTime.between(
+                Timestamp.valueOf(startOfDay),
+                Timestamp.valueOf(endOfDay)
+        ));
+
+        JPQLQuery<WorkLogsOutDTO> tupleQuery = query.select(
+                Projections.bean(WorkLogsOutDTO.class,
+                        workLogs.wlendTime,
+                        workLogs.wlworkStatus)
+        );
+
+        WorkLogsOutDTO result = tupleQuery.fetchOne();
+
+        return result;
+    }
 }
